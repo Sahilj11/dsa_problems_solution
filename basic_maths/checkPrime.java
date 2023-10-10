6f0cbf6f54fc1d4720905957677eb9fbafbfffb3
Sahil jalandhara <sahiljalandhara123@gmail.com>
2023-10-10 16:33:23 +0530

    checking prime

---
+++ basic_maths/checkPrime.java
@@ -0,0 +1,16 @@
+public class checkPrime {
+    public static void main(String[] args) {
+        int num = 11;
+        System.out.println(primeCheck(num));
+    }
+
+    static boolean primeCheck(int num) {
+        for (int i = 2; i < (int) Math.sqrt((double) num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
