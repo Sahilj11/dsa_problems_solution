241e5f863e07ee3dc476bd34c42e2f42d021040a
Sahil jalandhara <sahiljalandhara123@gmail.com>
2024-01-04 08:39:46 +0530

    1D BinarySearch

--- BinarySearch/1D/FirstLastArray.java
+++ BinarySearch/1D/FirstLastArray.java
@@ -4,33 +4,70 @@
 public class FirstLastArray {
 
     public static void main(String[] args) {
-        int[] arr = {2, 4, 6, 8, 8, 8, 11, 13};
-        brute(arr,8);
+        int[] arr = { 2, 4, 6, 8, 8, 8, 11, 13 };
+        optimul1(arr, 8);
     }
-    private static void brute(int[] arr,int x){
-        int occur = 0;
-        int[] index = {0,0};
+
+    private static void brute(int[] arr, int x) {
+        int[] index = { -1, -1 };
         for (int i = 0; i < arr.length; i++) {
-            if(occur > 0 && arr[i] == x){
+            if (index[0] == -1 && arr[i] == x) {
+                index[0] = i;
                 index[1] = i;
-            }else if(arr[i] == x && occur == 0){
-              occur++;
-              index[0] = i;
-           } 
+            } else if (index[0] != -1 && arr[i] == x) {
+                index[1] = i;
+            }
         }
         for (int i : index) {
-           System.out.println(i); 
+            System.out.println(i);
         }
     }
-    
-    private static void optimul1(int[] arr,int x){
-        int low = 0;
-        int high = arr.length-1;
-        int occur = 0;
-        int[] index = {0,0};
-        int midPoint = (low+high) / 2;
-        while (low <= high) {
- 
+
+    private static void optimul1(int[] arr, int x) {
+        int lb = lowerBound(arr, x);
+        int up = upperBound(arr, x);
+        if(lb == x || arr[lb] != x){
+            System.out.println(-1);
+            return;
         }
+        System.out.print(lb+",");
+        System.out.print(up-1);
+        System.out.println();
+    }
+
+    private static int lowerBound(int[] arr, int x) {
+        int low = 0;
+        int high = arr.length - 1;
+        int midPoint = (low + high) / 2;
+        int ans = 0;
+        while (low <= high) {
+            if (arr[midPoint] >= x) {
+                ans = midPoint;
+                high = midPoint - 1;
+            }
+            if (arr[midPoint] < x) {
+                low = midPoint + 1;
+            }
+            midPoint = (low + high) / 2;
+        }
+        return ans;
+    }
+
+    private static int upperBound(int[] arr, int x) {
+        int low = 0;
+        int high = arr.length - 1;
+        int midPoint = (low + high) / 2;
+        int ans = 0;
+        while (low <= high) {
+            if (arr[midPoint] > x) {
+                ans = midPoint;
+                high = midPoint - 1;
+            }
+            if (arr[midPoint] <= x) {
+                low = midPoint + 1;
+            }
+            midPoint = (low + high) / 2;
+        }
+        return ans;
     }
 }
