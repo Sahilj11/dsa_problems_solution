3b1346417b76ca0d4152a911c76daa55dd6000fd
Sahil jalandhara <sahiljalandhara123@gmail.com>
2024-01-16 13:02:48 +0530

    remove outerbracket problem completed

--- String/Easy/RemoveBracket.java
+++ String/Easy/RemoveBracket.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Stack;
 
 /**
@@ -6,8 +7,49 @@ import java.util.Stack;
 public class RemoveBracket {
 
     public static void main(String[] args) {
-        String s = "(()())(()))";
-        System.out.println(validString(s));
+        String s = "(()())(())(()(()))";
+        removeOuter(s);
+    }
+
+    private static String optimul(String s) {
+        int len = s.length();
+        if (len <= 2)
+            return "";
+        char[] c = s.toCharArray();
+        StringBuilder newString = new StringBuilder();
+        int open = 1;
+        int openLeft = 0;
+        for (int i = 1; i < len; i++) {
+            if (c[i] == '(') {
+                open++;
+                if (open > 1)
+                    newString.append('(');
+            } else {
+                if (open > 1)
+                    newString.append(')');
+                open--;
+            }
+        }
+        return newString.toString();
+    }
+
+    private static ArrayList<Character> removeOuter(String s) {
+        char[] para = s.toCharArray();
+        ArrayList<Character> ls = new ArrayList<>();
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < para.length; i++) {
+            st.push(para[i]);
+            if (st.size() == 2 && st.peek().equals(')')) {
+                st.clear();
+            } else if (st.size() >= 2) {
+                ls.add(st.peek());
+                if (st.peek().equals(')')) {
+                    st.pop();
+                    st.pop();
+                }
+            }
+        }
+        return ls;
     }
 
     private static boolean validString(String s) {
